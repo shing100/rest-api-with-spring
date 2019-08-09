def3e38f2530802169eb7f4ca0b8aaaadbe0e3a0
Lim geun <shing100@naver.com>
2019-08-09 23:40:24 +0900

    validate wrong data

--- src/test/java/com/kingname/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/kingname/demorestapi/events/EventControllerTest.java
@@ -37,7 +37,7 @@ public class EventControllerTest {
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
                 .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
-                .endEventDateTime(LocalDateTime.of(2018,11,25,14,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,21))
                 .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
                 .basePrice(100)
                 .maxPrice(200)
@@ -88,5 +88,37 @@ public class EventControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest());
 
+    }
+
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,24,14,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타일 펙토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
     }
 }
