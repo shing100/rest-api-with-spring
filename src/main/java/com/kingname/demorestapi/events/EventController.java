15d55b74d937e0e6a9f50d8ca18faba2ea78478e
Lim geun <shing100@naver.com>
2019-09-04 23:41:49 +0900

    index 만들기

--- src/main/java/com/kingname/demorestapi/events/EventController.java
+++ src/main/java/com/kingname/demorestapi/events/EventController.java
@@ -1,5 +1,6 @@
 package com.kingname.demorestapi.events;
 
+import com.kingname.demorestapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -35,12 +36,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -55,5 +56,9 @@ public class EventController {
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
 
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
---
+++ src/main/java/com/kingname/demorestapi/index/IndexController.java
@@ -0,0 +1,20 @@
+package com.kingname.demorestapi.index;
+
+import com.kingname.demorestapi.events.EventController;
+import lombok.var;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/api")
+    public ResourceSupport index() {
+        var index = new ResourceSupport();
+        index.add(linkTo(EventController.class).withRel("events"));
+        return index;
+    }
+}
